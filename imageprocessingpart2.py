501007401fbd6e593fe6f45c668ee108d7ccff4c
Daniel Bowdrey <daniel@Daniels-MacBook-Pro.local>
2019-01-24 16:54:36 +0000

    finished task xiii

--- imageprocessingpart2.py
+++ imageprocessingpart2.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 from scipy import misc
 from scipy.ndimage import interpolation, rotate
 
-lungs_image1 = misc.imread("/Users/daniel/coding-workspace/dicom/images/lungs.jpg")
-lungs_image2 = misc.imread("/Users/daniel/coding-workspace/dicom/images/lungs2.jpg")
+lungs_image1 = misc.imread("/Users/daniel/coding-workspace/dicom/images/lungs.jpg",flatten=True)
+lungs_image2 = misc.imread("/Users/daniel/coding-workspace/dicom/images/lungs2.jpg",flatten=True)
 
 #show each image on seperate plots
 plt.imshow(lungs_image1)
@@ -20,3 +20,22 @@ plt.show()
 plt.imshow(lungs_image1)
 plt.imshow(lungs_image2, alpha=0.3)
 plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+floating = ax.imshow(lungs_image2, cmap='RdPu')
+ax.imshow(lungs_image1,alpha=0.5, cmap='YlGn')
+
+# a function to shift the image given x and y coordinates
+def shiftImages(list_xy_shifts):
+    global floating
+    shifted_image = interpolation.shift(lungs_image2, list_xy_shifts, mode='nearest')
+    floating.set_data(shifted_image)
+    fig.canvas.draw()
+
+shiftImages([-25,-55])
+plt.show()
+
+
+
+
