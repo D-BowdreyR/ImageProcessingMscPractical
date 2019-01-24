b49011cf6ddc6705ad9a8505afdfc57bab6ea5a1
Daniel Bowdrey <daniel@Daniels-MacBook-Pro.local>
2019-01-24 15:57:20 +0000

    x. done plus addtional comments

--- imageprocessingpart2.py
+++ imageprocessingpart2.py
@@ -10,7 +10,13 @@ from scipy.ndimage import interpolation, rotate
 lungs_image1 = misc.imread("/Users/daniel/coding-workspace/dicom/images/lungs.jpg")
 lungs_image2 = misc.imread("/Users/daniel/coding-workspace/dicom/images/lungs2.jpg")
 
+#show each image on seperate plots
 plt.imshow(lungs_image1)
 plt.show()
 plt.imshow(lungs_image2)
+plt.show()
+
+#show both images on the same plot using transparency
+plt.imshow(lungs_image1)
+plt.imshow(lungs_image2, alpha=0.3)
 plt.show()
