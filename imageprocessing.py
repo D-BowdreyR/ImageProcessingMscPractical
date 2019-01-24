ae418949e6321176a0f01118ae0a19b1ea9b14a3
Daniel Bowdrey <daniel@Daniels-MacBook-Pro.local>
2019-01-24 15:21:02 +0000

    fix for image not being found

--- imageprocessing.py
+++ imageprocessing.py
@@ -6,21 +6,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import misc
 
-ct_image = misc.imread("images/CT.jpg", flatten=True)
+ct_image = misc.imread("/Users/daniel/coding-workspace/dicom/images/CT.jpg", flatten=True)
 plt.hist(ct_image.flatten(),bins=256, color='red')
 plt.show()
 
 #function to dispay the image at different window
 #levels
 def windowLevel(image,level,window,colormap):
-    #calculate vmin and vmax from window and level
-    vmx = level + (window)/2.0
+    vmx = level + (window/2.0)
     vmi = level - (window/2.0)
     plt.imshow(image,cmap=colormap,vmin=vmi,vmax=vmx)
-    return plt.show()
+    plt.show()
 
 
 windowLevel(ct_image,500,200,"Greys_r")
+
 
 
 
