963aa0bf9b6b1018d56949d5df1586beed6595d5
DanBR <47244055+D-BowdreyR@users.noreply.github.com>
2021-07-02 13:45:51 +0100

    Added shifting of image using keyboard event

--- files/Day5.py
+++ files/Day5.py
@@ -8,11 +8,15 @@ import matplotlib.pyplot as plt
 from scipy import misc
 from scipy.ndimage import interpolation, rotate
 from scipy import optimize
+import os
+import platform
 
-dcm_image1 = dcmread('../images/dcm/IMG-0004-00001.dcm')
-dcm_image2 = dcmread('../images/dcm/IMG-0004-00002.dcm')
-dcm_image3 = dcmread('../images/dcm/IMG-0004-00003.dcm')
-dcm_image4 = dcmread('../images/dcm/IMG-0004-00004.dcm')
+currentDir = os.getcwd()
+# read in the images
+dcm_image1 = dcmread(currentDir+'/images/dcm/IMG-0004-00001.dcm')
+dcm_image2 = dcmread(currentDir+'/images/dcm/IMG-0004-00002.dcm')
+dcm_image3 = dcmread(currentDir+'/images/dcm/IMG-0004-00003.dcm')
+dcm_image4 = dcmread(currentDir+'/images/dcm/IMG-0004-00004.dcm')
 
 # Fix for missing or incorrect Photometric Interpretation tag
 dcm_image1.PhotometricInterpretation = 'YBR_FULL'
@@ -20,15 +24,30 @@ dcm_image2.PhotometricInterpretation = 'YBR_FULL'
 dcm_image3.PhotometricInterpretation = 'YBR_FULL'
 dcm_image4.PhotometricInterpretation = 'YBR_FULL'
 
-
-plt.imshow(dcm_image1.pixel_array, cmap='Greens_r')
-plt.imshow(dcm_image2.pixel_array,alpha=0.3, cmap='Reds_r')
+# plot image 1 and 2 against each other
+fig1 = plt.figure(num='All Dicom Images')
+# plt.imshow(dcm_image1.pixel_array, cmap='Greens_r')
+# plt.imshow(dcm_image2.pixel_array, alpha=0.3, cmap='Reds_r')
+plt.imshow(dcm_image3.pixel_array, alpha=0.3, cmap='Blues_r')
+plt.imshow(dcm_image4.pixel_array, alpha=0.3, cmap='Purples_r')
 plt.show()
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+# create a new plot
+fig2 = plt.figure(num="Image Registration")
+ax = fig2.add_subplot(111)
+ax.set_title('DICOM Images overlayed', fontsize=18, fontweight='bold')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+txt = "Move the overlayed image using the arrow keys on your keyboard.\n \
+        To move quicker use the following key combinations \n \
+        Mac: cmd+arrowKey, Win/Linux: ctrl+arrowKey. \n \
+        Press the 'a' key to apply automated image registration"
+plt.figtext(0.5, 0.01, txt, wrap=True, horizontalalignment='center', fontsize=12)
+# add the images to the plot
 floating = ax.imshow(dcm_image2.pixel_array, cmap='Reds_r')
-ax.imshow(dcm_image1.pixel_array,alpha=0.5, cmap='Greens_r')
+ax.imshow(dcm_image1.pixel_array, alpha=0.5, cmap='Greens_r')
+currentpos = [0,0]
+
 #removed plt.show here to make the plot at the end work
 
 #def cost functions
@@ -42,17 +61,50 @@ def shiftwithcost(list_xy_shifts):
     shifted_image = interpolation.shift(dcm_image2.pixel_array, list_xy_shifts, mode='nearest')
     return CstFuncSumSq(dcm_image1.pixel_array,shifted_image)
 
+def getAutoRegisterImageShift():
+    # result of optimised image registration
+    return optimize.brute(shiftwithcost,((-50,50),(-50,50)),finish=optimize.fmin)
+
 #function to shift image given a tuple of x and y coordinates
 def shiftImages(list_xy_shifts):
     global floating
     shifted_image = interpolation.shift(dcm_image2.pixel_array,list_xy_shifts, mode='nearest')
     floating.set_data(shifted_image)
-    fig.canvas.draw()
+    fig2.canvas.draw()
 
-print(dcm_image1.pixel_array.shape)
+def handleKeyPress(event):
+    global currentpos
+    minIncrement = 1
+    maxIncrement = 10
+    
+    # handle key presses
+    if event.key == "right":
+        currentpos[1] += minIncrement
+    elif event.key == getKeyForOS()+"+right":
+        currentpos[1] += maxIncrement    
+    elif event.key == "left":
+        currentpos[1] -= minIncrement
+    elif event.key == getKeyForOS()+"+left":
+        currentpos[1] -= maxIncrement 
+    elif event.key == "up":
+        currentpos[0] -= minIncrement
+    elif event.key == getKeyForOS()+"+up":
+        currentpos[0] -= maxIncrement
+    elif event.key == "down":
+        currentpos[0] += minIncrement
+    elif event.key == getKeyForOS()+"+down":
+        currentpos[0] += maxIncrement
+    elif event.key == "a":
+        print(event.key)
+        currentpos = getAutoRegisterImageShift()
+    
+    shiftImages([currentpos[0], currentpos[1]])
 
-res = optimize.brute(shiftwithcost,((-50,50),(-50,50)),finish=optimize.fmin)
-print(res)
+def getKeyForOS():
+    if platform.system() == 'Darwin':
+        return "cmd"
+    else:
+        return "ctrl"
 
-shiftImages(res)
+fig2.canvas.mpl_connect('key_press_event',handleKeyPress)
 plt.show()
--- files/imageprocessingpart2.py
+++ files/imageprocessingpart2.py
@@ -4,11 +4,15 @@
 #import required libaries and functions
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import misc
+import imageio
 from scipy.ndimage import interpolation, rotate
+import os
 
-lungs_image1 = misc.imread("../images/lungs.jpg",flatten=True)
-lungs_image2 = misc.imread("../images/lungs2.jpg",flatten=True)
+currentDir = os.getcwd()
+
+lungs_image1 = imageio.imread(currentDir+"/images/lungs.jpg",as_gray=True)
+lungs_image2 = imageio.imread(currentDir+"/images/lungs2.jpg", as_gray=True)
+lungs_image3 = imageio.imread(currentDir+"/images/lungs3.jpg", as_gray=True)
 
 
 #show each image on seperate plots
@@ -16,24 +20,26 @@ plt.imshow(lungs_image1)
 plt.show()
 plt.imshow(lungs_image2)
 plt.show()
+plt.imshow(lungs_image3)
+plt.show()
 
 #show both images on the same plot using transparency
 plt.imshow(lungs_image1)
-plt.imshow(lungs_image2, alpha=0.3)
+plt.imshow(lungs_image3, alpha=0.3)
 plt.show()
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-floating = ax.imshow(lungs_image2, cmap='RdPu')
+floating = ax.imshow(lungs_image3, cmap='RdPu')
 ax.imshow(lungs_image1,alpha=0.5, cmap='YlGn')
 
 # a function to shift the image given x and y coordinates
-def shiftImages(list_xy_shifts):
+def shiftImages(list_xy_shifts, rotation_angle):
     global floating
-    shifted_image = interpolation.shift(lungs_image2, list_xy_shifts, mode='nearest')
+    shifted_image = interpolation.shift(rotate(lungs_image3,rotation_angle), list_xy_shifts, mode='nearest')
     floating.set_data(shifted_image)
     fig.canvas.draw()
 
-shiftImages([-25,-55])
+shiftImages([-25,-55], -5)
 plt.show()
 
